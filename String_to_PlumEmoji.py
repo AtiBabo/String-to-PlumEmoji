e4070534c44449a341c43255dd4c69cb974fdb01
itcomputer <96653318+itcomputer11@users.noreply.github.com>
2023-03-05 22:48:15 +0900

    Update String_to_PlumEmoji.py

--- String_to_PlumEmoji.py
+++ String_to_PlumEmoji.py
@@ -8,7 +8,7 @@ nomList = list("123467890")
 nomEngList = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]#이거 .split()으로 안 했다고 뭐라 하지마세요
 
 window=tkinter.Tk()
-window.title("String to Plum Emoji [0.0.2v]")
+window.title("String to Plum Emoji [0.0.3v]")
 window.geometry("640x100")
 window.resizable(False, False)
 
@@ -19,13 +19,14 @@ def copyDef(event):
     iptSt = list(entry.get())
     
     for i in range(0, len(iptSt)):
-        if iptSt[i].lower() in alphabetList:
-            if iptSt[i] == " ":
-                iptcom = iptcom + "  "
-            else:
-                iptcom = iptcom + ":" + iptSt[i].upper() + "_: "
-        elif str(iptSt[i]) in nomList:
-            iptcom = iptcom + ":" + nomEngList[int(iptSt[i])] + ": "
+        if iptSt[i].lower() in supportList:
+            if iptSt[i].lower() in alphabetList:
+                if iptSt[i] == " ":
+                    iptcom = iptcom + "  "
+                else:
+                    iptcom = iptcom + ":" + iptSt[i].upper() + "_: "
+            elif str(iptSt[i]) in nomList:
+                iptcom = iptcom + ":" + nomEngList[int(iptSt[i])] + ": "
         else:
             print("지원하지 않는 글자가 포함되어 있습니다.")
             infoLabel.config(text="지원하지 않는 글자가 포함되어 있습니다.", font=heheFont)
