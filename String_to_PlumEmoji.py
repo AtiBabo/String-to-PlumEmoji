efb24432a2db5c5ebb2f53d381b557bab1b5482e
itcomputer <96653318+itcomputer11@users.noreply.github.com>
2023-03-05 22:32:59 +0900

    Update String_to_PlumEmoji.py

--- String_to_PlumEmoji.py
+++ String_to_PlumEmoji.py
@@ -3,9 +3,12 @@ import tkinter
 import tkinter.font
 
 alphabetList = list("qwertyuiopasdfghjklzxcvbnm ")
+supportList = list("qwertyuiopasdfghjklzxcvbnm 1234567890")
+nomList = list("123467890")
+nomEngList = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]#이거 .split()으로 안 했다고 뭐라 하지마세요
 
 window=tkinter.Tk()
-window.title("String to Plum Emoji")
+window.title("String to Plum Emoji [0.0.2v]")
 window.geometry("640x100")
 window.resizable(False, False)
 
@@ -21,12 +24,14 @@ def copyDef(event):
                 iptcom = iptcom + "  "
             else:
                 iptcom = iptcom + ":" + iptSt[i].upper() + "_: "
+        elif str(iptSt[i]) in nomList:
+            iptcom = iptcom + ":" + nomEngList[int(iptSt[i])] + ": "
         else:
             print("지원하지 않는 글자가 포함되어 있습니다.")
             infoLabel.config(text="지원하지 않는 글자가 포함되어 있습니다.", font=heheFont)
             break
     
-    if iptSt[i].lower() in alphabetList:
+    if iptSt[i].lower() in supportList:
         print(iptcom)
         infoLabel.config(text=iptcom, font=heheFont)
         clipboard.copy(iptcom)
