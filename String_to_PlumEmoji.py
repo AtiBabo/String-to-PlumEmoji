58fd16f0515db8bb68d7f0c13c01c5d5db364d43
itcomputer <96653318+itcomputer11@users.noreply.github.com>
2023-03-05 23:12:50 +0900

    Update String_to_PlumEmoji.py

--- String_to_PlumEmoji.py
+++ String_to_PlumEmoji.py
@@ -3,12 +3,12 @@ import tkinter
 import tkinter.font
 
 alphabetList = list("qwertyuiopasdfghjklzxcvbnm ")
-supportList = list("qwertyuiopasdfghjklzxcvbnm 1234567890")
+supportList = list('qwertyuiopasdfghjklzxcvbnm 1234567890+-/*=?!*#×')
 nomList = list("123467890")
 nomEngList = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]#이거 .split()으로 안 했다고 뭐라 하지마세요
 
 window=tkinter.Tk()
-window.title("String to Plum Emoji [0.0.3v]")
+window.title("String to Plum Emoji [0.0.4v]")
 window.geometry("640x100")
 window.resizable(False, False)
 
@@ -21,12 +21,30 @@ def copyDef(event):
     for i in range(0, len(iptSt)):
         if iptSt[i].lower() in supportList:
             if iptSt[i].lower() in alphabetList:
-                if iptSt[i] == " ":
+                if iptSt[i] == " ":                                           #공백 
                     iptcom = iptcom + "  "
-                else:
+                else:                                                         #영어 
                     iptcom = iptcom + ":" + iptSt[i].upper() + "_: "
-            elif str(iptSt[i]) in nomList:
+            elif str(iptSt[i]) in nomList:                                    #숫자
                 iptcom = iptcom + ":" + nomEngList[int(iptSt[i])] + ": "
+            elif iptSt[i] == "+":                                             #+
+                iptcom = iptcom + ":heavy_plus_sign: "
+            elif iptSt[i] == "-":                                             #-
+                iptcom = iptcom + ":heavy_minus_sign: "
+            elif iptSt[i] == "/":                                             #/
+                iptcom = iptcom + ":heavy_division_sign: "
+            elif iptSt[i] == "×":                                             #×
+                iptcom = iptcom + ":heavy_multiplication_x: "
+            elif iptSt[i] == "*":                                             #*
+                iptcom = iptcom + ":asterisk: "
+            elif iptSt[i] == "=":                                             #=
+                iptcom = iptcom + ":heavy_equals_sign: "
+            elif iptSt[i] == "?":                                             #?
+                iptcom = iptcom + ":question: "
+            elif iptSt[i] == "!":                                             #!
+                iptcom = iptcom + ":exclamation: "
+            elif iptSt[i] == "#":                                             ##
+                iptcom = iptcom + ":hash: "
         else:
             print("지원하지 않는 글자가 포함되어 있습니다.")
             infoLabel.config(text="지원하지 않는 글자가 포함되어 있습니다.", font=heheFont)
